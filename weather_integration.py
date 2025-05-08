bf3b3e9d3e9b34dd45bcc3152cac4c05f98ba7dc
Vigyat singh <155925528+vigyat13@users.noreply.github.com>
2025-05-08 18:31:02 +0530

    Update weather_integration.py

--- weather_integration.py
+++ weather_integration.py
@@ -1,6 +1,6 @@
 import requests
 
 def get_weather():
-    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=YOUR_CITY&appid=22aa163fad24d67d2c4e8df5475b3f1a")
+    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=YOUR_CITY&appid=")
     data = response.json()
     return f"The weather in your city is {data['response'][0]['description']}."
