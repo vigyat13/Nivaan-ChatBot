9f7c9961331b6338296fa5b3c11b4ba708d3fad6
Vigyat singh <155925528+vigyat13@users.noreply.github.com>
2025-05-08 18:27:05 +0530

    Update voice_module.py

--- voice_module.py
+++ voice_module.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
 from elevenlabs import generate, play, set_api_key
 
-# Set your ElevenLabs API key
-set_api_key("sk_50bbd6ab0109ef5bf4a6d50fa7a905c90cbbd8bde47649ff")  # Replace this with your actual API key
+
+set_api_key("elevenlabs_api_key")  
 
 def listen_for_input():
     recognizer = sr.Recognizer()
@@ -21,7 +21,7 @@ def speak_with_eleven_labs(text):
     try:
         audio = generate(
             text=text,
-            voice="Charlie"  # You can choose other voices if you want
+            voice="Charlie"  
         )
         play(audio)
     except Exception as e:
